27c53194c53f45f4e99b471a98fa6d73ae88b9d4
Orso Meneghini <orso82@gmail.com>
2024-07-19 13:48:55 -0700

    fix syntax error

--- examples/plot_cooling_rates.jl
+++ examples/plot_cooling_rates.jl
@@ -5,7 +5,7 @@ impurities = [:Kr, :W, :Ar, :Ne, :C, :Si, :Cr]
 Lz = Dict(imp => ADAS.get_cooling_rates(imp) for imp in impurities);
 using Plots
 Log10Range(args...; kw...) = 10 .^ LinRange(args...; kw...)
-plot(layout=1, framestyle=:box,palette=)
+plot(layout=1, framestyle=:box,palette)
 for Lz_ in values(Lz)
     plot!(Lz_, Te=Log10Range(0, 4, 1000), ylim=[1e-35, 5e-31], yscale=:log10, layout=1)
 end
