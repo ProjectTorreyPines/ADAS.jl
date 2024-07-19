565728011ed1181498353516c392d0926de5757d
Orso Meneghini <orso82@gmail.com>
2024-07-19 13:48:55 -0700

    fix syntax error

--- examples/plot_cooling_rates.jl
+++ examples/plot_cooling_rates.jl
@@ -6,14 +6,9 @@ color_ = [:blue,:green, :red, :black,:cyan]
 Lz = OrderedDict(imp => ADAS.get_cooling_rates(imp) for imp in impurities);
 using Plots
 Log10Range(args...; kw...) = 10 .^ LinRange(args...; kw...)
-p = plot(layout=1, framestyle = :box, palette=:darktest, size=(400, 400))
-# for (i,Lz_) in enumerate(values(Lz))
-#     plot!(Lz_, Te=Log10Range(0.5, 4.5, 1000), xlim=[5.0,2e4], ylim=[1e-34, 5e-31], xscale=:log10, yscale=:log10, layout=1, color=color_[i])
-# end
-Te=Log10Range(0.5, 4.5, 100)
-
-for (i, Lz_) in enumerate(values(Lz))
-    plot!(Te, [Lz_.Lztot(1e20, Te_) for Te_ in Te]./Te.^(7/4), xlim=[5.0, 2e4], ylim=[1e-34, 5e-31], xscale=:log10, yscale=:log10, layout=1, color=color_[i], label=String(Lz_.imp))
+plot(layout=1, framestyle=:box,palette)
+for Lz_ in values(Lz)
+    plot!(Lz_, Te=Log10Range(0, 4, 1000), ylim=[1e-35, 5e-31], yscale=:log10, layout=1)
 end
 y = Log10Range(-34, -31, 4)
 x = Log10Range(1, 4, 4)
