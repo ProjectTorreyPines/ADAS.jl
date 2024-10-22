469bd84ba8676971381f5cbf9dcf726d28f0b3a4
Jerome Guterl <guterlj@fusion.gat.com>
2024-10-22 12:12:37 -0700

    fix jerome

--- examples/examples.jl
+++
@@ -1,5 +0,0 @@
-using ADAS
-build_ADAS_database()
-data = retrieve_ADAS_data("C"; year="latest", type="scd", metastable=false)
-show_ADAS_data("C")
-data = retrieve_ADAS_data("Si"; year="latest", type="acd", metastable=false)
