e2d302153e5755d177629acb7ad43b9c4b6cb0da
Jerome Guterl <guterlj@fusion.gat.com>
2024-11-21 05:54:14 -0800

    up examples

--- examples/plot_cooling_rates_Zeff.jl
+++ examples/plot_cooling_rates_Zeff.jl
@@ -1,8 +1,8 @@
 
 using ADAS, Plots, OrderedCollections, Format
-plotly()
+gr()
 impurities = [:Ne, :Ar, :Cr, :Kr,:W]
-color_ = [:blue, :green, :red, :black, :cyan]
+color_ = [:blue, :green, :red, :black, :gray]
 Lz = OrderedDict(imp => ADAS.get_cooling_rates(imp) for imp in impurities);
 Zeff = OrderedDict(imp => ADAS.get_Zeff(imp) for imp in impurities);
 using Plots
@@ -12,7 +12,7 @@ for (i,Lz_) in enumerate(values(Lz))
     plot!(Lz_, Te=Log10Range(0.5, 4.5, 1000), xlim=[5.0,2e4], ylim=[1e-34, 5e-31], xscale=:log10, yscale=:log10, layout=1, color=color_[i])
 end
 Te = Log10Range(0.5, 4.5, 1000)
-
+plot!()
 for (i, (Lz_,Zeff_)) in enumerate(zip(values(Lz),values(Zeff)))
     plot!([Zeff_(0.001, 1e20, Te_) for Te_ in Te], [Lz_.Lztot(1e20, Te_) for Te_ in Te], xlim=[1.0, 5], ylim=[1e-34, 5e-31], yscale=:log10, layout=1, color=color_[i], label=String(Lz_.imp))
 end
